b8549dae8f62c23ebfe855a8e55376e714b95edb
CWilliamsonJr <clarence.williamsonjr@gmail.com>
2017-02-09 07:07:19 -0500

    added a datagrid

--- Hearts_Of_Gold-WPF/Views/MainWindow.xaml.cs
+++ Hearts_Of_Gold-WPF/Views/MainWindow.xaml.cs
@@ -13,6 +13,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Hearts_Of_Gold_WPF.Models;
+using System.Data;
+using System.Data.Entity;
+//using System.Linq;
 
 namespace Hearts_Of_Gold_WPF
 {
@@ -25,6 +28,18 @@ namespace Hearts_Of_Gold_WPF
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+       
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            System.Windows.Data.CollectionViewSource itemViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("itemViewSource")));
+            // Load data by setting the CollectionViewSource.Source property:
+
+             itemViewSource.Source = _context.Items.Include(i => i.Donation_Categories.Categories).Include(i => i.Donation_Location.BusinessName).Include(i => i.User).ToList();
+            
         }
     }
 }
